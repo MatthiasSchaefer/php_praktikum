acb58a177ca504fa46e942544b360633d7a392fd
matthiasschaefer <schaefer_m@AMM9652W07.aerzener.de>
2014-04-10 12:12:00 +0200

    add method fak() (rekursiv)

--- Aufgaben/03Kontrollfluss.php
+++ Aufgaben/03Kontrollfluss.php
@@ -36,8 +36,35 @@ class Kontrollfluss {
         // Gib ein Array zurÃ¼ck das $a zahlen enthÃ¤lt, beginnend mit 1. [1,2,3,4,...,$a]
     }
 
-    public function fak(){
+/**
+     * Berechnet die Fakultät
+     * @return Fakultät von a, false falls a keine Zahl
+     */
+    public function fak()
+    {
+        if (!is_numeric($this->a))
+        {
+            return false;
+        }
+        return $this->fak_rek($this->a);
+    }
+    
+    /**
+     * rekursives Berechnen der Fakultät
+     * 
+     * @param $i Zahl zu der die Fakulktät berechnet werden soll
+     * @param string $erg Zwischenergebnis (wird automatisch benutzt)
+     * @return Fakultät von $i
+     */
+    private function fak_rek($i, $erg = NULL)
+    {
         // berechne rekursiv die fakultÃ¤t von $a
+        if ($i == 1)
+        {
+            return $erg == NULL ? 1 : $erg;
+        }
+        $erg = $erg == NULL ? $i-- : $erg * $i--;
+        return $this->fak_rek($i, $erg);
     }
 
 } 
